82f918f33d429e713b780f026da86e85229ce60f
aru2-star <68215528+aru2-star@users.noreply.github.com>
2021-02-06 15:43:10 +0530

    5 hour commit

---
+++ Test1/register1.js
@@ -0,0 +1,50 @@
+var local_array = [];
+var flag = false;
+
+function addUser() {
+    var name1 = document.getElementById('name').value;
+    var email1 = document.getElementById('email').value;
+    var pwd1 = document.getElementById('password').value;
+    var cpwd1 = document.getElementById('confirm_password').value;
+    var city1 = document.getElementById('city').value;
+    var state1 = document.getElementById('state').value;
+
+
+    var admin = {
+        name: name1,
+        email: email1,
+        password: pwd1,
+        city: city1,
+        state: state1
+    };
+
+    if (localStorage.getItem('local_array')) {
+        local_array = JSON.parse(localStorage.getItem('local_array'));
+    }
+
+    function register() {
+        for (var i = 0; i < local_array.length; ++i) {
+            var local = local_array[i];
+
+            if (local.email == email1) {
+                flag = true;
+                alert("Admin already registered");
+                break;
+            }
+            else {
+            }
+        }
+    }
+    register();
+    
+    if (flag === false) {
+        local_array.push(admin);
+        console.log(local_array);
+        localStorage.setItem("local_array", JSON.stringify(local_array));
+        var display = window.confirm("Registration Done");
+        if (display) {
+            window.location.href = "login.html";
+        }
+    }
+
+};

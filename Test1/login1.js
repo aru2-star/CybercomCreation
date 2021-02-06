22b9cebfecb058faaaebde012ee6ebcd46915c9a
aru2-star <68215528+aru2-star@users.noreply.github.com>
2021-02-06 13:38:54 +0530

    3 hour commit

---
+++ Test1/login1.js
@@ -0,0 +1,36 @@
+var local_array = [];
+var local;
+var flag = false;
+
+
+function login() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    if (localStorage.getItem('local_array')) {
+        local_array = JSON.parse(localStorage.getItem('local_array'));
+    }
+
+    function register() {
+        for (var i = 0; i < local_array.length; i++) {
+
+            local = local_array[i];
+
+            if (local_array[i].email == email && local_array[i].password == password) {
+                flag = true;
+
+                break;
+            }
+        }
+    }
+    register();
+
+    if (flag == false) {
+        flag = false;
+        alert("Invalid email and password!");
+    }
+    else {
+        sessionStorage.setItem("name", local.name);
+        window.location.href = "dashboard.html";
+    }
+};

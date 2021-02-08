694712dd1991a15adc218d58b278403ff0bc746f
aru2-star <68215528+aru2-star@users.noreply.github.com>
2021-02-09 00:16:36 +0530

    Rename Exercise5.js to JS Coding Challenge 5.js

---
+++ Code/JS_Coding_Challenge_5.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JS Coding Challenge 5</title>
+</head>
+<body>
+  <h1 id="code">Welcome!!!</h1>
+  <script type="text/javascript">
+    document.getElementById("code").onmouseover = function () { mouseOver() };
+    function mouseOver() {
+        alert("welcome to my WebPage!!")
+    }
+  </script>
+</body>
+</html>
